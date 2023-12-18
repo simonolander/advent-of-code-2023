d70f385b49e9297a65debccecf751956d26004f5
Simon Olander Sahlén <simon.olander-sahlen@regionstockholm.se>
2023-12-18 14:45:12 +0100

    Add application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "org.example"
@@ -16,4 +17,8 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass = "org.example.Main"
 }
